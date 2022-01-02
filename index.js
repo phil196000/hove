86d8239abf31cf0fb60b844736c50e42f88f035c
phil196000 <gumah196@gmail.com>
2022-01-02 19:36:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//QUESTION 1
+
+// const isEven = function (inputNumber) {
+//   // Write your code here
+// };
+const isEven = (inputNumber) => {
+  if (Number.isInteger(inputNumber)) {
+    return inputNumber % 2 === 0 ? true : false;
+  }
+};
+console.log(isEven(4));
+//QUESTION 2
+// * Question 2
+// This function should return true if inputNumber is does not have multiples beside 1 and itself only, otherwise return false
+// */
+// const isPrime = function(inputNumber) {
+// // Write your code here
+// }
+const isPrime = () => {};
